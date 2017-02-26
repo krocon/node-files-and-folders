ad721d3d104c17abf3e611af19c1ad9c2da17b37
Marc Kronberg <krocon@gmail.com>
2017-02-26 11:29:14 +0100

    update latest libs

--- demo.app.js
+++ demo.app.js
@@ -4,19 +4,18 @@
 
     //var fnf = require(__dirname + '/../files-and-folders-server/index.js');
     var fnf = require('files-and-folders-server');
-    var open = require('open');
+    var opn = require('opn');
 
     var port = 3002;
     
     console.info('app / __dirname :', __dirname);
     fnf.start({
         // auth: require('./demo.auth.js'), // optional
-        //clientRoot: __dirname + '/../files-and-folders-client/release',
         clientRoot: __dirname + '/node_modules/files-and-folders-client/release',
         port: port
     });
 
     var url = 'http://localhost:' + port + '/';
-    open(url);
+    opn(url);
 
 })();
